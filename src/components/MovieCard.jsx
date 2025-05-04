39a6de1144a7c835aec00bf0f3ee6aade6ae4bc7
UzairShekhani <uzairshekhani2017@gmail.com>
2025-05-04 17:56:27 +0500

    update

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -9,10 +9,12 @@ export default function MovieCard({ movie, toggleFavorite, isFavorite }) {
       <h3>{movie.Title}</h3>
       <p>{movie.Year}</p>
       <div className='card-actions'>
-        <Link to={`/movie/${movie.imdbID}`} className='details-link'>
-          Details
+        <Link to={`/movie/${movie.imdbID}`} className='custom-btn details'>
+           Details
         </Link>
-        <button onClick={() => toggleFavorite(movie)} className='fav-btn'>
+        <button
+          onClick={() => toggleFavorite(movie)}
+          className='custom-btn favorite'>
           {isFavorite ? "★ Remove" : "☆ Favorite"}
         </button>
       </div>
